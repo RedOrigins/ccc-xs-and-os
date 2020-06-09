75c649da1de4bc61d8f77bde0f46b37390537b26
RedOrigins <redst0ned314@gmail.com>
2020-06-09 12:13:07 +0100

    Added solution

---
+++ src/solution/index.js
@@ -0,0 +1,21 @@
+const io = require("socket.io-client");
+const socket = io.connect("http://localhost:3000");
+
+socket.emit("joinedGame", { gameId: "idhere" });
+
+socket.on("gameOver", (data) => {
+  console.log(data);
+});
+
+takeTurn("x", 0);
+takeTurn("o", 8);
+takeTurn("x", 1);
+takeTurn("o", 3);
+takeTurn("x", 2);
+
+function takeTurn(piece, tile) {
+  socket.emit("takeTurn", {
+    piece,
+    tile,
+  });
+}
